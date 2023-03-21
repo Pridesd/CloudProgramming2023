65392917469a5c2303da8e1dab36432dc207a6b8
Pridesd <whwotjr98@naver.com>
2023-03-21 17:23:51 +0900

    detail page

--- do_it_django_prj/blog/urls.py
+++ do_it_django_prj/blog/urls.py
@@ -5,4 +5,5 @@ from . import views #현재 위치에 있는 views를 가지고 옴
 
 urlpatterns = [
     path('', views.index),
+    path('<int:post_num>/', views.singlePostPage),
 ]
--- do_it_django_prj/blog/views.py
+++ do_it_django_prj/blog/views.py
@@ -7,8 +7,18 @@ def index(request):
     posts = Post.objects.all().order_by('-pk') #-를 붙이면 내림차순으로 정렬
     return render(
         request,
-        'blog/index.html',
+        'blog/index.html', #자동으로 templates로 연결됨
         {
             'posts': posts
+        }
+    )
+def singlePostPage(request, post_num):
+    post = Post.objects.get(pk = post_num)
+
+    return render(
+        request,
+        'blog/singlePostPage.html',
+        {
+            'post': post
         }
     )
