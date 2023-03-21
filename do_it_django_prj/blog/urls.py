b25e762f933c8cbf56a6f829bdd44a660ac0977e
Pridesd <whwotjr98@naver.com>
2023-03-21 16:55:34 +0900

    view test

---
+++ do_it_django_prj/blog/urls.py
@@ -0,0 +1,8 @@
+## urlconf for blog
+from django.urls import path
+
+from . import views #현재 위치에 있는 views를 가지고 옴
+
+urlpatterns = [
+    path('', views.index),
+]
--- do_it_django_prj/blog/views.py
+++ do_it_django_prj/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def index(request):
+    posts = Post.objects.all().order_by('-pk') #-를 붙이면 내림차순으로 정렬
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': posts
+        }
+    )
