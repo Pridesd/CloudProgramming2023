ebe0de3381dbd058a20d253f3f79c9e3ceb5e585
Pridesd <whwotjr98@naver.com>
2023-03-28 17:03:46 +0900

    static and media

--- do_it_django_prj/blog/models.py
+++ do_it_django_prj/blog/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+
+    head_image = models.ImageField(upload_to= 'blog/images/%Y/%M/%D/' ,blank=True) #blank는 필수항목이 아니라는 뜻임 연월일 별로 폴더를 만들에서 제작함
+    file_upload = models.FileField(upload_to= 'blog/files/%Y/%M/%D/' ,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
