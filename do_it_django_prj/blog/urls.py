1fdc9809233d03975ed9f19a10667294750dacae
Pridesd <whwotjr98@naver.com>
2023-03-28 15:39:34 +0900

    clase based view

--- do_it_django_prj/blog/urls.py
+++ do_it_django_prj/blog/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views #현재 위치에 있는 views를 가지고 옴
 
 urlpatterns = [
-    path('', views.index),
-    path('<int:post_num>/', views.singlePostPage),
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
 ]
--- do_it_django_prj/blog/views.py
+++ do_it_django_prj/blog/views.py
@@ -1,24 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
 # Create your views here.
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk') #-를 붙이면 내림차순으로 정렬
-    return render(
-        request,
-        'blog/index.html', #자동으로 templates로 연결됨
-        {
-            'posts': posts
-        }
-    )
-def singlePostPage(request, post_num):
-    post = Post.objects.get(pk = post_num)
-
-    return render(
-        request,
-        'blog/singlePostPage.html',
-        {
-            'post': post
-        }
-    )
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk') #-를 붙이면 내림차순으로 정렬
+#     return render(
+#         request,
+#         'blog/post_list.html', #자동으로 templates로 연결됨
+#         {
+#             'posts': posts
+#         }
+#     )
+# def singlePostPage(request, post_num):
+#     post = Post.objects.get(pk = post_num)
+#
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post': post
+#         }
+#     )
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+class PostDetail(DetailView):
+    model = Post
