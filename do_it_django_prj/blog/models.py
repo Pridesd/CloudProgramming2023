6c9ae51237bef8060db24236b15e0feb5bd3231b
Pridesd <whwotjr98@naver.com>
2023-04-04 16:14:29 +0900

    포스트에 카테고리 넣기

--- do_it_django_prj/blog/models.py
+++ do_it_django_prj/blog/models.py
@@ -3,6 +3,21 @@ from django.contrib.auth.models import User
 
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)  # 유일한 값을 가져야함
+    slug = models.SlugField(max_length=50, unique=True, allow_unicode=True)
+
+    # 유니코드(한글)의 사용도 가능케 함
+    # 슬러그는 키워드를 나타냄 읽을 수 있는 텍스트로 URL로 만들 때 사용
+
+    # 카테고리 이름을 찾아오게끔 함
+    def __str__(self):
+        return self.name
+
+    # 어드민 페이지에서 문법에 맞게 보이게 함
+    class Meta:
+        verbose_name_plural = "Categories"
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -15,22 +30,13 @@ class Post(models.Model):
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     #author가 없어지면 게시글도 삭제
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    #카테고리를 가지고 옴
 
     def __str__(self):
         return f'[{self.pk}]-{self.title} - {self.author}'
-    def get_absolute_url(self):
+    def get_absolute_url(self): #절대 경로를 얻게 해줌
         return f'/blog/{self.pk}'
     def get_file_name(self):
         return os.path.basename(self.file_upload.name) #파일 이름만 나타나게 하는 함수
-class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True) #유일한 값을 가져야함
-    slug = models.SlugField(max_length=50, unique=True, allow_unicode=True)
-    #유니코드(한글)의 사용도 가능케 함
-    # 슬러그는 키워드를 나타냄 읽을 수 있는 텍스트로 URL로 만들 때 사용
-    
-    #카테고리 이름을 찾아오게끔 함
-    def __str__(self):
-        return self.name
 
-    class Meta:
-        verbose_name_plural= "Categories"
