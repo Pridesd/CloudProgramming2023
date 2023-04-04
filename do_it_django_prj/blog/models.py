7f313b20b6a2094a14135045afc879bbd6dcfd2b
Pridesd <whwotjr98@naver.com>
2023-04-04 15:43:13 +0900

    카테고리 추가

--- do_it_django_prj/blog/models.py
+++ do_it_django_prj/blog/models.py
@@ -22,3 +22,12 @@ class Post(models.Model):
         return f'/blog/{self.pk}'
     def get_file_name(self):
         return os.path.basename(self.file_upload.name) #파일 이름만 나타나게 하는 함수
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True) #유일한 값을 가져야함
+    slug = models.SlugField(max_length=50, unique=True, allow_unicode=True)
+    #유니코드(한글)의 사용도 가능케 함
+    # 슬러그는 키워드를 나타냄 읽을 수 있는 텍스트로 URL로 만들 때 사용
+    
+    #카테고리 이름을 찾아오게끔 함
+    def __str__(self):
+        return self.name
